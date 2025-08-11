b424ca99b029138ce5fe14deba7c1ec202b107fb
harsha <harshaharrison2003@gmail.com>
2025-08-11 16:27:35 +0530

    initial commit

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4_Om1zCUrnrOim_OnWnwdMVMHNg3DvHQ",
+  authDomain: "ecosteps-259a9.firebaseapp.com",
+  projectId: "ecosteps-259a9",
+  storageBucket: "ecosteps-259a9.firebasestorage.app",
+  messagingSenderId: "199603477584",
+  appId: "1:199603477584:web:5dec5304cf5daf738e4555",
+  measurementId: "G-QSSPSXEK2W"
+};
+firebase.initializeApp(firebaseConfig);
